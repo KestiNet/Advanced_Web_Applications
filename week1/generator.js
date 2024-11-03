d226980e949e5063713748c97e49a45897908c48
KestiNet <esakesti@gmail.com>
2024-11-03 23:39:12 +0000

    bug fixes

--- week1/generator.js
+++ week1/generator.js
@@ -1,12 +1,49 @@
+function createWikiItem(breed, imageUrl, wikiText) {
+    const mainContainer = document.querySelector('.container');
+
+    const wikiItem = document.createElement('div');
+    wikiItem.className = 'wiki-item';
+
+    const wikiHeader = document.createElement('h1');
+    wikiHeader.textContent = breed;
+    wikiHeader.className = 'wiki-header';
+
+    const wikiContent = document.createElement('div');
+    wikiContent.className = 'wiki-content';
+
+    const wikiParagraph = document.createElement('p');
+    wikiParagraph.textContent = wikiText;
+    wikiParagraph.className = 'wiki-text';
+
+    const imgContainer = document.createElement('div');
+    imgContainer.className = 'img-container';
+
+    const wikiImg = document.createElement('img');
+    wikiImg.src = imageUrl;
+    wikiImg.alt = `Image of ${breed}`;
+    wikiImg.className = 'wiki-img';
+
+    imgContainer.appendChild(wikiImg);
+    wikiContent.appendChild(wikiParagraph);
+    wikiContent.appendChild(imgContainer);
+
+    wikiItem.appendChild(wikiHeader);
+    wikiItem.appendChild(wikiContent);
+
+    mainContainer.appendChild(wikiItem);
+
+    console.log(`Displayed content for ${breed}`);
+}
 
 
-function fetchDogPictures(numImages = 1) {
-    const url = `https://dog.ceo/api/breeds/image/random/${numImages}`;
 
-    fetch(url)
+function fetchDogImages(numImages = 1) {
+    const apiUrl = `https://dog.ceo/api/breeds/image/random/${numImages}`;
+
+    fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            console.log('Fetched data:', data.message);
+            console.log('Fetched images:', data.message);
 
             if (data.message.length === 0) {
                 console.log('No images received');
@@ -14,82 +51,39 @@ function fetchDogPictures(numImages = 1) {
             }
 
             data.message.forEach(imageUrl => {
-                const breed = breeedName(imageUrl);
-                const breedFormat = breeder(breed);
+                const breedName = extractBreedName(imageUrl);
+                const formattedBreed = formatBreedName(breedName);
 
-                fetchWikiPedia(breedFormat)
-                    .then(wikiContent => {
-                        template(breedFormat, imageUrl, wikiContent);
+                fetchWikiData(formattedBreed)
+                    .then(wikiText => {
+                        createWikiItem(formattedBreed, imageUrl, wikiText);
                     });
             });
-        })
-      
+        });
 }
 
-function breeedName(url) {
+function extractBreedName(url) {
     const parts = url.split('/');
-    const breedPart = parts[parts.length - 2];
+    const breedSegment = parts[parts.length - 2];
 
-    return breedPart.includes('-') ? breedPart.split('-').reverse().join(' ') : breedPart.replace('-', ' ');
+    return breedSegment.includes('-') ? breedSegment.split('-').reverse().join(' ') : breedSegment.replace('-', ' ');
 }
 
-function breeder(breed) {
-
+function formatBreedName(breed) {
     return breed
         .split(' ')
         .map(word => word.charAt(0).toUpperCase() + word.slice(1))
         .join(' ');
 }
 
-function fetchWikiPedia(breed) {
-    const apiUrl = `https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(breed)}`;
+function fetchWikiData(breed) {
+    const wikiApiUrl = `https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(breed)}`;
 
-    return fetch(apiUrl)
-        .then(response => {
-     
-            return response.json();
-        })
-        .then(data => data.extract)
-
-}
-
-function template(breed, imageUrl, wikiContent) {
-    const container = document.querySelector('.container');
-
-    const wikiDiv = document.createElement('div');
-    wikiDiv.className = 'wiki-content';
-
-    const imgContainer = document.createElement('div');
-    imgContainer.className = 'img-container';
-
-    const image = document.createElement('img');
-    image.src = imageUrl;
-    image.alt = `Image of ${breed}`;
-    image.className = 'breed-image'; 
-
-    imgContainer.appendChild(image);
-
-    const wikiTextDiv = document.createElement('div');
-    wikiTextDiv.className = 'wiki-text';
-
-    const header = document.createElement('h1');
-    header.textContent = breed;
-    header.className = 'breed-header'; 
-
-    const description = document.createElement('p');
-    description.textContent = wikiContent;
-    description.className = 'description'; 
-
-    wikiTextDiv.appendChild(header);
-    wikiTextDiv.appendChild(description);
-
-    wikiDiv.appendChild(imgContainer);
-    wikiDiv.appendChild(wikiTextDiv);
-
-    container.appendChild(wikiDiv);
-
-    console.log(`Displayed content for ${breed}`);
+    return fetch(wikiApiUrl)
+        .then(response => response.json())
+        .then(data => data.extract);
 }
 
 
-fetchDogPictures(5);
+
+fetchDogImages(5);
