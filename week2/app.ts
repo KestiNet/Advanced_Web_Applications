41186b3c3fa44468bc03bb96353a0485a6432bf9
KestiNet <esakesti@gmail.com>
2024-11-06 23:59:26 +0000

    week2 Interfaces

--- week2/app.ts
+++ week2/app.ts
@@ -17,3 +17,52 @@ let Vehicle = {
 }
 
 console.log(Vehicle)
+
+interface IVehicle {
+    model: string,
+    color: string,
+    year: number,
+    power: number
+}
+
+interface ICar extends IVehicle{
+    bodyType: string,
+    wheelCount: number
+}
+
+interface IPlane extends IVehicle{
+    wingspan: number
+}
+
+interface IBoat extends IVehicle{
+    draft: number
+}
+
+let Car: ICar = {
+    model: "Ford focus", 
+    color: "Green", 
+    year: 2016, 
+    power: 150, 
+    bodyType: "Hatchback", 
+    wheelCount: 4 
+}
+
+let Plane: IPlane = {
+    model: "Boeing 777", 
+    color: "White", 
+    year: 2020, 
+    power: 170000, 
+    wingspan: 65 
+}
+
+let Boat: IBoat = {
+    model: "Bella", 
+    color: "Black", 
+    year: 2022, 
+    power: 100, 
+    draft: 0.42 
+}
+
+console.log(Car)
+console.log(Plane)
+console.log(Boat)
--- week2/dist/app.js
+++ week2/dist/app.js
@@ -7,3 +7,28 @@ let Vehicle = {
     power: 60
 };
 console.log(Vehicle);
+let Car = {
+    model: "Ford focus",
+    color: "Green",
+    year: 2016,
+    power: 150,
+    bodyType: "Hatchback",
+    wheelCount: 4
+};
+let Plane = {
+    model: "Boeing 777",
+    color: "White",
+    year: 2020,
+    power: 170000,
+    wingspan: 65
+};
+let Boat = {
+    model: "Bella",
+    color: "Black",
+    year: 2022,
+    power: 100,
+    draft: 0.42
+};
+console.log(Car);
+console.log(Plane);
+console.log(Boat);
